6d3f648db35a91ee43eb8cf333b31d6debc6aac2
root <root@epsilon-agent-packer.k3s>
2020-08-31 01:44:19 +0300

    create ami-agent

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-export K3S_HOST=controlplane.amerah.dobc
+export K3S_HOST=controlplane.epsilon.dobc
 export K3S_TOKEN=$(cat /home/ubuntu/token)
 export K3S_URL=https://$K3S_HOST:6443
 curl -sfL https://get.k3s.io | sh -
